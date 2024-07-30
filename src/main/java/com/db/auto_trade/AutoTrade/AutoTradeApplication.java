b19d9210f9e6f55925050072bb78bfaaae4ed1a5
Christopher Aust <chris.aust94@gmail.com>
2024-07-30 02:14:05 -0700

    add spring init template

---
+++ src/main/java/com/db/auto_trade/AutoTrade/AutoTradeApplication.java
@@ -0,0 +1,13 @@
+package com.db.auto_trade.AutoTrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoTradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoTradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/db/auto_trade/AutoTrade/AutoTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.auto_trade.AutoTrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoTradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

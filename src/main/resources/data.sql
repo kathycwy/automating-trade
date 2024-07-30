4f90f862829c6f08e95dfed7d8590055eb347304
Kathy Chan <wingyin97606@gmail.com>
2024-07-30 04:34:46 -0700

    add db init script

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS Orders;
+CREATE TABLE Orders(id INT PRIMARY KEY, type VARCHAR(255), price INT, quantity INT);
+INSERT INTO Orders VALUES(1, 'Buy', 10, 5);
+INSERT INTO Orders VALUES(2, 'Sell', 15, 2);
+
+DROP TABLE IF EXISTS Clients;
+CREATE TABLE Clients(id INT PRIMARY KEY, name VARCHAR(255), acctNo INT, balance INT);
+INSERT INTO Clients VALUES(1, 'Peter', 1001, 2000);
+INSERT INTO Clients VALUES(2, 'David', 1002, 3500);
+
+DROP TABLE IF EXISTS Transactions;
+CREATE TABLE Transactions(id INT PRIMARY KEY, orderId INT, buyerId INT, sellerId INT, timestamp TIMESTAMP, status VARCHAR(255));
+INSERT INTO Transactions VALUES(1, 1, 1001, 1002, CURRENT_TIMESTAMP, 'Completed');
+INSERT INTO Transactions VALUES(2, 2, 1002, 1001, CURRENT_TIMESTAMP, 'Pending');
